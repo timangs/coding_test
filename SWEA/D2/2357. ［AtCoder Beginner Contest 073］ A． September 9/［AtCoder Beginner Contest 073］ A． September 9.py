cd4f0473240689b91c031e4f425de6049413afad
timang <jeongkyu1145@gmail.com>
2023-12-06 20:23:33 +0900

    [D2] Title: [AtCoder Beginner Contest 073] A. September 9, Time: 135 ms, Memory: 58,320 KB -BaekjoonHub

---
+++ SWEA/D2/2357. ［AtCoder Beginner Contest 073］ A． September 9/［AtCoder Beginner Contest 073］ A． September 9.py
@@ -0,0 +1,67 @@
+# 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+# 아래 표준 입출력 예제 필요시 참고하세요.
+
+# 표준 입력 예제
+'''
+a = int(input())                        정수형 변수 1개 입력 받는 예제
+b, c = map(int, input().split())        정수형 변수 2개 입력 받는 예제 
+d = float(input())                      실수형 변수 1개 입력 받는 예제
+e, f, g = map(float, input().split())   실수형 변수 3개 입력 받는 예제
+h = input()                             문자열 변수 1개 입력 받는 예제
+'''
+
+# 표준 출력 예제
+'''
+a, b = 6, 3
+c, d, e = 1.0, 2.5, 3.4
+f = "ABC"
+print(a)                                정수형 변수 1개 출력하는 예제
+print(b, end = " ")                     줄바꿈 하지 않고 정수형 변수와 공백을 출력하는 예제
+print(c, d, e)                          실수형 변수 3개 출력하는 예제
+print(f)                                문자열 1개 출력하는 예제
+'''
+
+
+
+
+'''
+      아래의 구문은 input.txt 를 read only 형식으로 연 후,
+      앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
+      여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
+      아래 구문을 이용하면 이후 입력을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
+
+      따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 구문을 사용하셔도 좋습니다.
+      아래 구문을 사용하기 위해서는 import sys가 필요합니다.
+
+      단, 채점을 위해 코드를 제출하실 때에는 반드시 아래 구문을 지우거나 주석 처리 하셔야 합니다.
+'''
+#import sys
+#sys.stdin = open("input.txt", "r")
+
+first_input_int: int = int(input())
+if first_input_int >= 1:
+    if first_input_int <= 90:
+        pass
+    else:
+        print("not 1 <= N <= 90")
+else:
+    print("not 1 <= N <= 90")
+
+second_input_list: list = []
+for i in range(first_input_int):
+    inner_int:int = int(input())
+    if inner_int >= 10:
+        if inner_int <= 99:
+            second_input_list.append(inner_int)
+        else:
+            print("not 10 <= N <= 99")
+    else:
+        print("not 10 <= N <= 99")
+
+for_i:int = 0
+for i in second_input_list:
+    for_i+=1
+    if str(i)[0] == "9" or str(i)[1] == "9":
+        print(f"#{for_i} Yes")
+    else:
+        print(f"#{for_i} No")
